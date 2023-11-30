5e78f7cea3403343c19996f6c58d558f284484fa
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-11-30 22:58:02 +0200

    update: update

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -16,7 +16,7 @@ const SearchInput: FC<ISearchInput> = ({ searchTerm, handleSearch }) => {
         onChange={handleSearch}
         type="text"
         id="search_id"
-        className="w-[300px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-[300px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
         placeholder="Folder..."
       />
     </div>
---
+++ src/helpers/index.ts
@@ -0,0 +1,3 @@
+export { default as deleteItemById } from './deleteItemById.ts';
+export { default as filterData } from './filterData.ts';
+export { default as toggleExpended } from './toggleExpended.ts';
